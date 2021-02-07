f1b93091d60d91bd1b3bf9cf976c92c338223f28
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-07 09:59:36 -0800

    Results test passing

---
+++ src/components/results/Results.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Results from './Results';
+
+describe('Results component', () => {
+  afterEach(() => cleanup());
+  it('renders Results', () => {
+    const { asFragment } = render(<Results results="results" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
